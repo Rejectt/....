f4e01815f94c59ee47d09f6a85ee2a37463d5946
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-12-21 12:32:13 -0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`star Bot- Script By : medo `);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers ' Script By : n3k4a  Codes ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setStatus("dnd")
+ 
+});
+
+
+client.login(process.env.BOT_TOKEN);
