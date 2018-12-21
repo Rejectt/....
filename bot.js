6a6e9b715a891b7d3b61cd26dba8e60424dc108e
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-12-21 12:44:34 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-
-
-
-client.on('ready',  () => {
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
-  console.log('by medo');
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-  console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-  console.log('is online')
-client.user.setStatus("dnd");
+const prefix = "!"
+client.on('ready', () => {
+  console.log('---------------');
+  console.log(' Bot Is Online')
+  console.log('---------------')
 });
-      
 
 client.login(process.env.BOT_TOKEN);
